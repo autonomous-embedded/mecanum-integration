5e469d7c99d5323dfc9da5f0a6082f0d4ea5655c
nmpluta <pluta.natalia.m@gmail.com>
2024-01-31 00:46:22 +0100

    updated baseline, working now :)

--- mecanum-hl-run.sh
+++ mecanum-hl-run.sh
@@ -2,5 +2,5 @@
 
 source /home/orinnx3/repos_ES_CPS/devel/setup.bash
 
-roslaunch mecanum_bringup mecanum_vision.launch initial_reset:=True enable_accel:=False enable_color:=True enable_confidence:=False enable_depth:=False enable_gyro:=False enable_infra1:=False enable_infra2:=False enable_pointcloud:=False enable_pose:=False enable_sync:=False
+roslaunch mecanum_bringup mecanum_vision.launch initial_reset:=True enable_accel:=False enable_color:=True enable_confidence:=False enable_depth:=False enable_gyro:=False enable_infra1:=False enable_infra2:=False enable_pointcloud:=False enable_pose:=False enable_sync:=False color_width:=640 color_height:=480
 
